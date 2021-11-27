edb04b84c988ae8bbcbc47fbdcf095dc89b87a24
notBillJames <charmillion@gmail.com>
2021-11-26 22:40:54 -0700

    fixed workability issues

--- scoring.py
+++ scoring.py
@@ -52,7 +52,7 @@ class Player:
         bid = input(f'What does {self.name} bid? ')
         while not bid.isdigit():
             print('Must enter a digit')
-            bid = input('How many will be playing today? ')
+            bid = input(f'What does {self.name} bid? ')
         round_data['Bid'] = int(bid)
         if round_data['Bid'] == card_count:
             print('Backalley?')
@@ -68,32 +68,33 @@ class Player:
         return bid_df
 
     def score_round(self):
-        result = int(input(f'How many tricks did {self.name} win?'))
+        result = int(input(f'How many tricks did {self.name} win? '))
         self.bids.fillna(result, inplace=True)
         self.score = self.bids.copy()
         self.score['Score'] = self.score.apply(score_func, axis=1)
         self.score = self.score[['Round #', 'Card Count',
                                  'Player', 'Score']]
+        return self.score
 
 
 class Game():
     def __init__(self):
-        player_count = input('How many will be playing today? ')
-        while not player_count.isdigit():
+        self.player_count = input('How many will be playing today? ')
+        while not self.player_count.isdigit():
             print('Must enter a digit')
-            player_count = input('How many will be playing today? ')
-        player_count = int(player_count)
-        players = []
-        for i in range(player_count):
+            self.player_count = input('How many will be playing today? ')
+        self.player_count = int(self.player_count)
+        self.players = []
+        for i in range(self.player_count):
             name = input(f'Please enter a name for Player {i + 1}: ')
-            players.append(Player(name))
-        self.players = [print(i) for i in players]
-        self.max_hand = 54 // len(players)
+            self.players.append(Player(name))
+        self.max_hand = 54 // self.player_count
         if self.max_hand >= 7.0:
             self.max_hand = 7.0
         self.round_number = 1
         self.count = 1
         self.ascending = True
+        print(f'Welcome, play to {self.max_hand} cards.')
 
     def card_count(self):
         if self.ascending:
@@ -128,7 +129,7 @@ class Game():
             score_df = player.score_round()
             scores.append(score_df)
         scores = pd.concat(scores)
-        scores = scores.groupby('Player').Score.sum()
-        scores = scores.sort_values(ascending=False)
+        scores = scores.groupby('Player').Score.sum().reset_index()
+        scores = scores.sort_values(by='Score', ascending=False)
         self.players = self.change_order()
         return scores
